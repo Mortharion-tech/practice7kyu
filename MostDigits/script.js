df3afebe57cc93687b4ffbd4bb3b78b5a7e768aa
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-09-25 22:10:12 +0200

    Most digits

---
+++ MostDigits/script.js
@@ -0,0 +1,7 @@
+/* Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array. */
+
+function findLongest(array) {
+    return Number(array.map((num)=>num.toString()).sort((a,b)=>b.length-a.length)[0]);
+}
